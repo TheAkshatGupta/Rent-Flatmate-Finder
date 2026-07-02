47ff054cb80260dda147dc05f09ba9dc8d7ae214
Akshat Gupta <2k23.csds2310601@gmail.com>
2026-07-02 20:04:48 +0530

    Setup Flask application architecture

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
 
+from config import Config
+from extensions import db, login_manager
+
+
 def create_app():
+
     app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    login_manager.init_app(app)
 
     @app.route("/")
     def home():
-        return "Rent & Flatmate Finder Backend Running Successfully!"
+        return "<h1>Rent & Flatmate Finder Backend Running Successfully 🚀</h1>"
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- backend/config.py
+++ backend/config.py
@@ -1,2 +1,8 @@
+import os
+
 class Config:
-    SECRET_KEY = "your-secret-key"
+    SECRET_KEY = os.getenv("SECRET_KEY", "rent-flatmate-secret-key")
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///rent_flatmate.db"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
